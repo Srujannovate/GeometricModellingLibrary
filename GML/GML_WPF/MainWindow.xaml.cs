8f4bc7473cc6a359d2b666159972cab7d9c5433d
Rohan-Deore <deorerohan.2612@gmail.com>
2026-03-23 12:09:13 +0530

    3D model rendering implemented

--- GML/GML_WPF/MainWindow.xaml.cs
+++ GML/GML_WPF/MainWindow.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Media.Media3D;
+using Hx = HelixToolkit.Wpf.SharpDX;
+using HxCore = HelixToolkit.SharpDX.Core;
+using HxSharpDX = HelixToolkit.SharpDX;
+using HxGeom = HelixToolkit.Geometry;
+using System.Numerics;
+using System.Collections.Generic;
+using HT = HelixToolkit;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GML_WPF
 {
@@ -16,9 +16,69 @@ namespace GML_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Hx.PerspectiveCamera _camera = new Hx.PerspectiveCamera
+        {
+            Position = new Point3D(0, 0, 8),
+            LookDirection = new Vector3D(0, 0, -8),
+            UpDirection = new Vector3D(0, 1, 0),
+            FieldOfView = 45
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize DirectX manager and camera in code-behind
+            View3D.EffectsManager = new HxSharpDX.DefaultEffectsManager();
+            View3D.Camera = _camera;
+
+            // Bind gestures explicitly: left=rotate, right=pan
+            View3D.InputBindings.Add(new MouseBinding(Hx.ViewportCommands.Rotate, new MouseGesture(MouseAction.LeftClick)));
+            View3D.InputBindings.Add(new MouseBinding(Hx.ViewportCommands.Pan, new MouseGesture(MouseAction.RightClick)));
+
+            // Add a cube to the scene by constructing geometry directly
+            var positions = new HT.Vector3Collection
+            {
+                new Vector3(-1,-1,-1), new Vector3(1,-1,-1), new Vector3(1,1,-1), new Vector3(-1,1,-1),
+                new Vector3(-1,-1, 1), new Vector3(1,-1, 1), new Vector3(1,1, 1), new Vector3(-1,1, 1)
+            };
+            var indices = new HT.IntCollection
+            {
+                // front (-Z)
+                0,1,2, 0,2,3,
+                // right (+X)
+                1,5,6, 1,6,2,
+                // back (+Z)
+                5,4,7, 5,7,6,
+                // left (-X)
+                4,0,3, 4,3,7,
+                // top (+Y)
+                3,2,6, 3,6,7,
+                // bottom (-Y)
+                4,5,1, 4,1,0
+            };
+
+            var cubeMesh = new HxSharpDX.MeshGeometry3D
+            {
+                Positions = positions,
+                Indices = indices
+            };
+
+            var cubeModel = new Hx.MeshGeometryModel3D
+            {
+                Geometry = cubeMesh,
+                Material = Hx.PhongMaterials.Orange
+            };
+
+            View3D.Items.Add(cubeModel);
+
+            // Optional: handle menu actions like reset camera
+            ResetCameraMenuItem.Click += (_, __) =>
+            {
+                _camera.Position = new Point3D(0, 0, 8);
+                _camera.LookDirection = new Vector3D(0, 0, -8);
+                _camera.UpDirection = new Vector3D(0, 1, 0);
+            };
         }
     }
 }
